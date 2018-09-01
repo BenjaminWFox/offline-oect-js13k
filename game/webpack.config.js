13006af8e5f5fb75c5be46dde0bdc16e9ea92fc9
Ben Fox <benjaminwfox@gmail.com>
2018-08-31 18:07:33 -0700

    Updated webpack. Added logs checking for unused code

---
+++ game/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: {
+    ga: './src/ga.js',
+    main: './src/main.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: false,
+          compress: {
+            drop_console: true,
+            keep_infinity: true,
+          },
+          output: {
+            comments: false,
+            beautify: false,
+          },
+        }
+      })
+    ]
+  }
+};
