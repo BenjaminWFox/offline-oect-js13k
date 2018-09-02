8842db072d48de857fa108fa7ad6009a8c72df37
Ben Fox <benjaminwfox@gmail.com>
2018-09-02 14:45:29 -0700

    Remove unused files. Label unlinked code _

--- assets/canvases-astar/js/lib/toolbox.js
+++
@@ -1,4 +0,0 @@
-(function(){"use strict";var Toolbox=window.Toolbox={};var ctor=function(){};var inherits=function(parent,protoProps,staticProps){var child;if(protoProps&&protoProps.hasOwnProperty('constructor')){child=protoProps.constructor;}else{child=function(){return parent.apply(this,arguments);};}
-_.extend(child,parent);ctor.prototype=parent.prototype;child.prototype=new ctor();if(protoProps)_.extend(child.prototype,protoProps);if(staticProps)_.extend(child,staticProps);child.prototype.constructor=child;child.__super__=parent.prototype;return child;};function extendThis(protoProps,staticProps){var child=inherits(this,protoProps,staticProps);child.extend=extendThis;return child;}
-Toolbox.Base=function(){}
-Toolbox.Base.extend=extendThis;})();
